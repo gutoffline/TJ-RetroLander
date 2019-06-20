e649e269a3c37544b38fc3f0b94a20f3d53f62e5
Guto Xavier <gutoffline@gmail.com>
2019-06-19 21:30:22 -0300

    player

---
+++ Retro Lander/Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform player;
+    public float zoomMinimo = 15f;
+    public float zoomMaximo = 4f;
+    public float intervalo = 0.1f;
+
+    private Camera minhaCamera;
+    private float velocidadeZoom;
+    private Vector3 velocidadeMovimento;
+    
+	void Start () {
+        minhaCamera = GetComponent<Camera>();
+	}
+	
+	void Update () {
+		if(player != null) {
+            if(player.position.y > 0) {
+                minhaCamera.orthographicSize = Mathf.SmoothDamp(minhaCamera.orthographicSize, zoomMinimo, ref velocidadeZoom, intervalo);
+            }
+            else {
+                Vector3 novaPosicao = new Vector3(player.position.x, player.position.y, -10);
+                transform.position = Vector3.SmoothDamp(transform.position, novaPosicao, ref velocidadeMovimento, intervalo);
+                minhaCamera.orthographicSize = Mathf.SmoothDamp(minhaCamera.orthographicSize, zoomMaximo, ref velocidadeZoom, intervalo);
+            }
+        }
+	}
+}
---
+++ Retro Lander/Assets/Scripts/GameController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject gameHUD;
+    public GameObject menuPause;
+    public GameObject menuGameOver;
+    public GameObject menuGameWin;
+    private bool isPaused;
+    private void Awake()
+    {
+        Time.timeScale = 1;
+    }
+
+    void Start()
+    {
+        isPaused = false;
+        menuPause.SetActive(false);
+        menuGameWin.SetActive(false);
+        menuGameOver.SetActive(false);
+    }
+
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Return)){
+            PauseManager();
+        }
+    }
+
+    private void PauseManager(){
+        isPaused = !isPaused;
+        if(isPaused){
+            OnPaused();
+        }else{
+            OnResume();
+        }
+    }
+
+    private void OnPaused(){
+        menuPause.SetActive(true);
+        gameHUD.SetActive(false);
+        Time.timeScale = 0;
+    }
+
+    private void OnResume(){
+        menuPause.SetActive(false);
+        gameHUD.SetActive(true);
+        Time.timeScale = 1;
+    }
+
+    private void OnGameOver(){
+        menuGameOver.SetActive(true);
+        gameHUD.SetActive(false);
+        Time.timeScale = 0;
+    }
+
+    public void OnGameWin(){
+        menuGameWin.SetActive(true);
+        gameHUD.SetActive(false);
+    }
+
+    public void Resume(){
+        OnResume();
+    }
+
+    public void Restart(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Sair(){
+        SceneManager.LoadScene("Intro");
+    }
+}
